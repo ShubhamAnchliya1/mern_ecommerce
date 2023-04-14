69633ff150a9490c2ff5768a8e314148e1fa4885
ShubhamAnchliya1 <shubham.anchliya@iflair.com>
2023-04-14 19:08:47 +0530

    ourstore ui

---
+++ src/components/Color.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Color() {
+  return (
+    <>
+        <ul className="colors   ps-0">
+            <li className='my-2'></li>
+            <li className='my-2'></li>
+            <li className='my-2'></li>
+            <li className='my-2'></li>
+        </ul>
+    </>
+  )
+}
+
+export default Color;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import product1 from "../images/watch.jpg";
 import product12 from "../images/watch-1.avif";
 import ReactStars from "react-rating-stars-component";
@@ -8,53 +8,60 @@ import prodcompare from "../images/prodcompare.svg";
 import view from "../images/view.svg";
 import addcart from "../images/add-cart.svg";
 
-function ProductCard() {
+function ProductCard(props) {
+  const { grid } = props;
+  // console.log(grid);
+
+  let location = useLocation();
+
   const ratingChanged = (newRating) => {
     console.log(newRating);
   };
 
   return (
-    // <div className="col-3">
-    <Link className="product-card position-relative">
-      <div className="wishlist-icon position-absolute">
-        <Link>
-          <img src={wish} alt="" />
-        </Link>
-      </div>
-      <div className="product-image">
-        <img src={product1} alt="" className="img-fluid" />
-        <img src={product12} alt="" className="img-fluid" />
-      </div>
-      <div className="product-details">
-        <h6 className="brand">Fire Bolt</h6>
-        <h5 className="product-title">
-          A beautiful sunday morning renaissance
-        </h5>
-        <ReactStars
-          count={5}
-          onChange={ratingChanged}
-          size={24}
-          value={4}
-          edit={false}
-          activeColor="#ffd700"
-        />
-        <p className="price">₹950</p>
-      </div>
-      <div className="action-bar position-absolute">
-        <div className="d-flex flex-column gap-15">
+    <div
+      className={` ${location.pathname === "/ourstore" ? `col-${grid}` : "col-3"}`}
+    >
+      <Link className="product-card position-relative">
+        <div className="wishlist-icon position-absolute">
           <Link>
-            <img src={prodcompare} alt="prodcompare" />
-          </Link>
-          <Link>
-            <img src={view} alt="view" />
-          </Link>
-          <Link>
-            <img src={addcart} alt="addcart" />
+            <img src={wish} alt="" />
           </Link>
         </div>
-      </div>
-    </Link>
-    // </div>
+        <div className="product-image">
+          <img src={product1} alt="" className="img-fluid" />
+          <img src={product12} alt="" className="img-fluid" />
+        </div>
+        <div className="product-details">
+          <h6 className="brand">Fire Bolt</h6>
+          <h5 className="product-title">
+            A beautiful sunday morning renaissance
+          </h5>
+          <ReactStars
+            count={5}
+            onChange={ratingChanged}
+            size={24}
+            value={4}
+            edit={false}
+            activeColor="#ffd700"
+          />
+          <p className="price">₹950</p>
+        </div>
+        <div className="action-bar position-absolute">
+          <div className="d-flex flex-column gap-15">
+            <Link>
+              <img src={prodcompare} alt="prodcompare" />
+            </Link>
+            <Link>
+              <img src={view} alt="view" />
+            </Link>
+            <Link>
+              <img src={addcart} alt="addcart" />
+            </Link>
+          </div>
+        </div>
+      </Link>
+    </div>
   );
 }
 
